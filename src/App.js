413b290082f7994d41a6bd5a145af284999beaa6
wtmcwhor <wtmcwhor@asu.edu>
2024-03-06 14:17:16 +0800

    add cart logic

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import CartProvider from './store/CartProvider';
+import { useState } from 'react';
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  function showCartHandler() {
+    setCartIsShown(true);
+  }
+
+  function hideCartHandler() {
+    setCartIsShown(false);
+  }
   return (
     <CartProvider>
-      <Header />
+      {cartIsShown && <div>Cart...</div>}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
